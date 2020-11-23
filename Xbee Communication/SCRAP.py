535f7c1fe639dcc4cc9893e0458a87ae4d1ad79d
mraaronnelson <nelson.aaronr@gmail.com>
2020-11-23 14:38:07 -0600

    Update SCRAP.py

--- Xbee Communication/SCRAP.py
+++ Xbee Communication/SCRAP.py
@@ -36,19 +36,68 @@ grid = np.nan * np.empty((len(j),len(i)))
 
 # Bin the data onto a 10x10 grid
 # Have to reverse x & y due to row-first indexing
-zi, yi, xi = np.histogram2d(j, i, bins=(1910,1910), weights=z, normed=False)
-counts, _, _ = np.histogram2d(j, i, bins=(1910,1910))
+# zi, yi, xi = np.histogram2d(j, i, bins=(1910,1910), weights=z, normed=False)
+# counts, _, _ = np.histogram2d(j, i, bins=(1910,1910))
 
-zi = zi / counts
-zi = np.ma.masked_invalid(zi)
+# zi = zi / counts
+# zi = np.ma.masked_invalid(zi)
 
-fig, ax = plt.subplots()
-ax.pcolormesh(xi, yi, zi, edgecolor='black', cmap='jet')
-scat = ax.scatter(i, j, c=z, s=50, cmap='jet', edgecolors='black')
-fig.colorbar(scat)
-ax.margins(0.05)
+# fig, ax = plt.subplots()
+# ax.pcolormesh(xi, yi, zi, edgecolor='black', cmap='jet')
+# scat = ax.scatter(i, j, c=z, s=50, cmap='jet', edgecolors='black')
+# fig.colorbar(scat)
+# ax.margins(0.05)
 
+# plt.show()
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+X, Y = np.meshgrid(x, y)
+
+#Z = 4*X**2 + Y**2
+
+# zr = []
+for i in range(0, len(df.magy)):
+    y = df.magy.min() + ((df.magy.max() - df.magy.min()) * len(df.magy)*float(i))
+    for k in range(0, len(df.magx)):
+        x = df.magx.min() + ((df.magx.max() - df.magx.min()) * len(df.magx)*float(k))
+
+#         v = 4.0*x*x + y*y
+
+#         zr.append(v)
+
+# Z = np.reshape(zr, X.shape)
+Z = np.reshape(z, X.shape)
+
+print(X.shape)
+print(Y.shape)
+print(Z.shape)
+
+plt.contour(X, Y, Z)
 plt.show()
+
+xout = pd.DataFrame(X)
+yout = pd.DataFrame(Y)
+zout = pd.DataFrame(Z)
+
+xout = xout.to_csv('/users/aaron/desktop/x2d.csv')
+yout = yout.to_csv('/users/aaron/desktop/y2d.csv')
+zout = zout.to_csv('/users/aaron/desktop/z2d.csv')
+
+
+X = pd.read_csv('/users/aaron/desktop/x2d.csv', header=None)
+X = X.drop(0, axis=1)
+X = X[1:]
+
+Y = pd.read_csv('/users/aaron/desktop/y2d.csv', header=None)
+Y = Y.drop(0, axis=1)
+Y = Y[1:]
+
+Z = pd.read_csv('/users/aaron/desktop/z2d.csv', header=None)
+Z = Z.drop(0, axis=1)
+Z = Z[1:]
 
 # # width 
 
@@ -124,24 +173,24 @@ z axis tick labels.
 # 
 # 
 
-# from mpl_toolkits.mplot3d import axes3d
-# import matplotlib.pyplot as plt
-# from matplotlib import cm
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
-# fig = plt.figure()
-# ax = fig.gca(projection='3d')
-# # X, Y, Z = axes3d.get_test_data(0.05)
-# ax.plot_surface(x, y, zi, rstride=8, cstride=8, alpha=0.3)
-# cset = ax.contour(x, y, zi, zdir='z', offset=-100, cmap=cm.coolwarm)
-# cset = ax.contour(x, y, zi, zdir='x', offset=-40, cmap=cm.coolwarm)
-# cset = ax.contour(x, y, zi, zdir='y', offset=40, cmap=cm.coolwarm)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+# X, Y, Z = axes3d.get_test_data(0.05)
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, alpha=0.3)
+cset = ax.contour(X, Y, Z, zdir='z', offset=-8, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='x', offset=-8, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='y', offset=8, cmap=cm.coolwarm)
 
-# ax.set_xlabel('X')
-# ax.set_xlim(-40, 40)
-# ax.set_ylabel('Y')
-# ax.set_ylim(-40, 40)
-# ax.set_zlabel('Z')
-# ax.set_zlim(-100, 100)
+ax.set_xlabel('X')
+ax.set_xlim(-8, 48)
+ax.set_ylabel('Y')
+ax.set_ylim(-8, 8)
+ax.set_zlabel('Z')
+ax.set_zlim(-8, 8)
 
 # plt.show()
 
